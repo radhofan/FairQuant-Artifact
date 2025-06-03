1fb2198fe5e061881475dd39ea20de89ec60fa63
Radhofan <radhofanazizi@gmail.com>
2025-06-03 14:29:02 +0700

    ada

--- FairQuant/adult.sh
+++ FairQuant/adult.sh
@@ -16,15 +16,15 @@ else
     exit 1
 fi
 
-# Just test the loop
-for ((i=1; i<=12; i++)); do
-    echo -e "\n-----Running network AC-$i on $PA-----"
-    model_path="./FairQuant-Artifact/models/adult/AC-$i.nnet"
-    echo "Model Path: $model_path"
-done
-
-# for ((i=1; i<=12; i++)); do # for each model 1 to 12
+# # Just test the loop
+# for ((i=1; i<=12; i++)); do
 #     echo -e "\n-----Running network AC-$i on $PA-----"
-#     # ./network_test "../models/adult/AC-$i.nnet" "$idx"
-#     ./FairQuant-Artifact/FairQuant/network_test "./FairQuant-Artifact/models/adult/AC-$i.nnet" "$idx"
+#     model_path="./FairQuant-Artifact/models/adult/AC-$i.nnet"
+#     echo "Model Path: $model_path"
 # done
+
+for ((i=1; i<=12; i++)); do # for each model 1 to 12
+    echo -e "\n-----Running network AC-$i on $PA-----"
+    # ./network_test "../models/adult/AC-$i.nnet" "$idx"
+    ./FairQuant-Artifact/FairQuant/network_test "./FairQuant-Artifact/models/adult/AC-$i.nnet" "$idx"
+done
