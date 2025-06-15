60c7a7bb0a62ce6f46ef19e0018568cff8b36721
Radhofan <radhofanazizi@gmail.com>
2025-06-15 11:24:48 +0700

    ada

--- FairQuant/split.c
+++ FairQuant/split.c
@@ -124,13 +124,21 @@ const char* decode_feature(int feature_index, float value) {
     int idx = (int)round(value);
     switch (feature_index) {
         case 1: return workclass_map[idx];
-        case 3: return education_map[idx];
-        case 5: return marital_status_map[idx];
-        case 6: return occupation_map[idx];
-        case 7: return relationship_map[idx];
-        case 8: return sex_map[idx];
-        case 9: return race_map[idx];
-        case 12: return native_country_map[idx];
+        case 2: return education_map[idx];
+        case 3: return marital_status_map[idx];
+        case 4: return occupation_map[idx];
+        case 5: return relationship_map[idx];
+        case 6: return sex_map[idx];
+        case 7: return race_map[idx];
+        case 10: return native_country_map[idx];
+        // case 1: return workclass_map[idx];
+        // case 3: return education_map[idx];
+        // case 5: return marital_status_map[idx];
+        // case 6: return occupation_map[idx];
+        // case 7: return relationship_map[idx];
+        // case 8: return sex_map[idx];
+        // case 9: return race_map[idx];
+        // case 12: return native_country_map[idx];
         default: {
             static char buffer[32];
             snprintf(buffer, sizeof(buffer), "%.0f", value);
