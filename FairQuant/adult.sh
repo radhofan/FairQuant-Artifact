203152f418379116ac83769d45d04804d1988b8f
Radhofan <radhofanazizi@gmail.com>
2025-07-24 10:35:12 +0700

    ada

--- FairQuant/adult.sh
+++ FairQuant/adult.sh
@@ -41,5 +41,5 @@ fi
 # ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/adult/AC-14.nnet" "$idx"        # Done
 # ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/adult/AC-14-Biased.nnet" "$idx" # Done
 
-./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/adult/AC-15.nnet" "$idx"        #
-# ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/adult/AC-15-Biased.nnet" "$idx" #
+# ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/adult/AC-15.nnet" "$idx"        #
+./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/adult/AC-15-Biased.nnet" "$idx" #
