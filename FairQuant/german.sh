c43029daf9545e459991dd1e87d76c8e6fb4bfd5
Radhofan <radhofanazizi@gmail.com>
2025-07-29 21:53:22 +0700

    ada

--- FairQuant/german.sh
+++ FairQuant/german.sh
@@ -30,4 +30,4 @@ fi
 #     ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/german/GC-$i.nnet" "$idx"
 # done
 
-./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/german/GC-8.nnet" "$idx"
+./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/german/GC-9.nnet" "$idx"
