70a305f2c9b6f8c83cb13f411ad5cc611488cbbc
Radhofan <radhofanazizi@gmail.com>
2025-08-07 11:50:35 +0700

    ada

--- FairQuant/bank.sh
+++ FairQuant/bank.sh
@@ -28,4 +28,4 @@ fi
 #     ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/bank/BM-$i.nnet" "$idx"
 # done
 
- ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/bank/BM-6.nnet" "$idx"
+ ./FairQuant-Artifact/FairQuant/network_test "FairQuant-Artifact/models/bank/BM-7.nnet" "$idx"
